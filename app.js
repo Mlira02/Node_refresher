fb7e3f781bee11fc643d9ec8959d69b8fc7607bb
marcos <marcos@pop-os.localdomain>
2021-10-05 12:02:08 -0500

    Added more refresher code

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-let name = "Max";
-console.log(name);
-const age = 29;
-let hasHobbies = true;
-console.log(`My name is ${name} and i am ${age} years old`);
+const http = require("http");
+const fs = require("fs");
 
-let hobbies = ["Sports", "Cooking"];
-for(let hobby of hobbies){
-    console.log(hobby);
-}
+const server = http.createServer((req, res) => {
+    // console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    if(url === "/")
+    {
+        res.write("<html>");
+        res.write("<head><title>Enter Message</title></head>");
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Submit</button></form></body>");
+        res.write("</html");
+        return res.end();
+    }
+    if(url === "/message" && method === "POST")
+    {
+        const body = [];
+        req.on("data", (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split("=")[1];
+            fs.writeFile("message.txt", message, (err) => {
+                res.statusCode = 302;
+                res.setHeader("Location", "/");
+                return res.end();
+            });
+        });
+    }
+    // Process.exit ends our process for the server...
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My first node response</title></head>");
+    res.write("<body><h1>This is the resopnse here!!!</h1></body>");
+    res.write("</html");
+    res.end();
+});
 
-let newHobbies = [...hobbies, "Something new here"];
-console.log(newHobbies);
+server.listen(3000);
