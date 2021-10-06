42fb87370f5539838815e444a2265978bc3ed819
marcos <marcos@pop-os.localdomain>
2021-10-06 11:59:21 -0500

    Added routes and html files for pages

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use("/add-product", (req, res, next) => {
-    // console.log("In the add product middleware.....");
-    res.send("<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Submit</button></form>");
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "./", "views", "page-not-found.html"));
 });
 
-app.post("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-    // console.log("In the base middleware.....");
-    res.send("<h1>Hello from the Express server!!</h1>");
-});
 
 app.listen(3000);
